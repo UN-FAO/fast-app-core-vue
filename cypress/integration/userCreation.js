16926ecfada7c65189a24935eb9195b2b12cc5c9
Ignacio Cabrera <cabrerabywaters@gmail.com>
2017-11-01 18:10:51 +0100

    Fixing test

--- cypress/integration/userCreation.js
+++ cypress/integration/userCreation.js
@@ -1,35 +1,29 @@
-describe('Create new user', function() {
-	it('Visits login page', function() {
+describe('New user creation', function() {
+	it('Creates a new user', function() {
     	cy.visit('http://localhost:8080/')
     	cy.contains('Version')
-  	})
-	it('Clicks the new user link', function() {
+    	// Clicks the new user link
     	cy.contains('New user ?').click({force:true})
    	 	cy.contains('Register')
- 	})
- 	it('Fills the new user information', function() {
-    	cy.get('input[name="data[email]"]')
+   	 	//Fills the new user information
+   	 	cy.get('input[name="data[email]"]')
       	.type('fake@email.com')
       	.should('have.value', 'fake@email.com')
       	cy.get('input[name="data[password]"]')
       	.type('123')
       	.should('have.value', '123')
- 	})	
- 	it('Click Register', function() {
-    	cy.get('button[name="data[submit]"]')
+      	// Click Register
+      	cy.get('button[name="data[submit]"]')
       	.click()
- 	})
- 	it('Click confirmation message', function() {
-    	cy.contains('Are you sure?')
+      	// Click confirmation message
+      	cy.contains('Are you sure?')
     	cy.contains('Yes, send it!').click()
- 	})
- 	it('Send the submition for new User', function() {
+    	// Send the submition for new User
     	cy.contains('Sent!')
     	cy.contains('OK').click()
- 	})
- 	it('Redirect the user to login', function() {
+    	// Redirect the user to login
     	cy.url().should('include', '/login')
     	cy.contains('Version')
- 	})
+  	})
 })
 
