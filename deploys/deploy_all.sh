863937efe51d176948ab580064d73af6a0655a80
Ignacio Cabrera <cabrerabywaters@gmail.com>
2017-11-01 16:49:50 +0100

    Zip with the version

--- deploys/deploy_all.sh
+++ deploys/deploy_all.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
+####################################################
+#  Project Setup Variables
+###################################################
+	APPNAME="FAST"
+	PRODUCTNAME="SHARP"
+	VERSION="0.1.12"
+	AUTHOR="Ignacio Cabrera <ignacio.cabrera@fao.org>"
+	DESCRIPTION="Collection application for Form.io"
+###################################################
 quasar build
-sh ./deploys/cordova/deploy_all.sh && sh ./deploys/electron/deploy_all.sh && sh ./deploys/web/deploy_all.sh
+sh ./deploys/cordova/deploy_all.sh && \
+sh ./deploys/electron/deploy_all.sh && \
+sh ./deploys/web/deploy_all.sh && \
+rm -rf ./deploys/all && \
+mkdir ./deploys/all && \
+cp ./cordova/platforms/android/build/outputs/apk/android-debug.apk ./deploys/all/${PRODUCTNAME}_v${VERSION}.apk && \
+cp -R ./electron/dist/${PRODUCTNAME}-darwin-x64/${PRODUCTNAME}.app ./deploys/all/${PRODUCTNAME}_v${VERSION}.app && \
+cp -R ./dist ./deploys/all/${PRODUCTNAME}_v${VERSION}
+zip -r ${PRODUCTNAME}_v${VERSION}.zip ./deploys/all/*
+mv ${PRODUCTNAME}_v${VERSION}.zip ./deploys/all/${PRODUCTNAME}_v${VERSION}.zip
