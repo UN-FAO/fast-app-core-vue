69872652e3510e76788dc1c7a2364a34bcd2d07a
Ignacio Cabrera <cabrerabywaters@gmail.com>
2018-03-07 19:12:32 +0100

    Show LD functionallity

--- src/layout/components/pageLinks.vue
+++ src/layout/components/pageLinks.vue
@@ -1,6 +1,6 @@
 <template>
 <div style="color:black">
-   <q-side-link multiline highlight item :to="{name: 'pageManager', params: {pageId: page.pagesPageUrl}}" :key="page.pagesPageUrl" v-for="page in pages">
+   <q-side-link multiline highlight item v-if="page.SHOW_LD" :to="{name: 'pageManager', params: {pageId: page.pagesPageUrl}}" :key="page.pagesPageUrl" v-for="page in pages">
       <q-item-side :icon="page.pageIcon" />
       <q-item-main :label="$t(page.pageTitle)" class="text-white"/>
     </q-side-link>
