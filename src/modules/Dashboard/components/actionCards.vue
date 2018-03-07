545bf8a9250319cda6459d5cbdf826e41131e15f
Ignacio Cabrera <cabrerabywaters@gmail.com>
2018-03-07 14:59:06 +0100

    Finish Page configuration from Form.io

---
+++ src/modules/Dashboard/components/actionCards.vue
@@ -0,0 +1,90 @@
+<template>
+<div>
+<div class="row" v-for="(chunk, index) in _elements" v-bind:key="index" style="padding-top:30px">
+     <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12" v-for="element in chunk" v-bind:key="element.innerCardsTitle" style="padding-top:30px">
+      <q-card>
+      <q-card-title>
+        <q-item>
+          <q-item-side :avatar="element.innerCardsAvatar" />
+          <q-item-main>
+            <q-item-tile label>{{element.innerCardsTitle}}</q-item-tile>
+
+          </q-item-main>
+        </q-item>
+              <q-card-separator />
+        <span slot="subtitle" class="pull-left">
+          {{element.innerCardsSubtitle}}
+          <br>
+        <q-btn color="primary" @click="$router.push(element.action)" v-if="action.innerCardsActionsTarget" v-for="action in element.innerCardsActions" v-bind:key="action.innerCardsActionsText">
+          {{action.innerCardsActionsText}}
+        </q-btn>
+        </span>
+      </q-card-title>
+    </q-card>
+     </div>
+
+   </div>
+</div>
+</template>
+<script>
+import _chunk from "lodash/chunk";
+import {
+  QCard,
+  QCardMedia,
+  QItemSide,
+  QCardTitle,
+  QCardSeparator,
+  QCardMain,
+  QFab,
+  QFabAction,
+  QFixedPosition,
+  QPullToRefresh,
+  QTabs,
+  QTab,
+  QTabPane,
+  QCollapsible,
+  QBtn,
+  QIcon,
+  QTooltip,
+  QList,
+  QItem,
+  QItemSeparator
+} from "quasar";
+export default {
+  name: "actioncards",
+  props: ["page"],
+  watch: {
+    page: function(val) {
+      console.log("val val val ", val);
+    }
+  },
+  components: {
+    QCard,
+    QCardMedia,
+    QItemSide,
+    QCardTitle,
+    QCardSeparator,
+    QCardMain,
+    QFab,
+    QFabAction,
+    QFixedPosition,
+    QPullToRefresh,
+    QTabs,
+    QTab,
+    QTabPane,
+    QCollapsible,
+    QBtn,
+    QIcon,
+    QTooltip,
+    QList,
+    QItem,
+    QItemSeparator
+  },
+  computed: {
+    _elements() {
+      console.log('this.page', this.page)
+      return _chunk(this.page.innerCards, 3);
+    }
+  }
+};
+</script>
--- src/modules/Dashboard/views/pageManager.vue
+++ src/modules/Dashboard/views/pageManager.vue
@@ -1,33 +1,12 @@
 <template>
-  <div class="container-fluid col-lg-offset-1 col-lg-10" >
+  <div class="container-fluid col-lg-offset-1 col-lg-10" v-bind:key="$route.path" >
     <div class="section-title" style="font-size: 30px;padding-top:30px">
-      Manage System Page Manager
+      {{page.pageTitle}}
     </div>
     <hr style="border-top: 1px solid #7f7f7f;">
-   <div class="row" v-for="(chunk, index) in chunckedElements" v-bind:key="index" style="padding-top:30px">
-     <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12" v-for="element in chunk" v-bind:key="element.innerCardsTitle" style="padding-top:30px">
-      <q-card>
-      <q-card-title>
-        <q-item>
-          <q-item-side :avatar="element.innerCardsAvatar" />
-          <q-item-main>
-            <q-item-tile label>{{element.innerCardsTitle}}</q-item-tile>
 
-          </q-item-main>
-        </q-item>
-              <q-card-separator />
-        <span slot="subtitle" class="pull-left">
-          {{element.innerCardsSubtitle}}
-          <br>
-        <q-btn color="primary" @click="$router.push(element.action)" v-if="action.innerCardsActionsTarget" v-for="action in element.innerCardsActions" v-bind:key="action.innerCardsActionsText">
-          {{action.innerCardsActionsText}}
-        </q-btn>
-        </span>
-      </q-card-title>
-    </q-card>
-     </div>
+    <actioncards :page="page" v-bind:key="$route.path"></actioncards>
 
-   </div>
   </div>
 
 </template>
@@ -58,28 +37,12 @@ import {
   QItem,
   QItemSeparator
 } from "quasar";
-import _chunk from "lodash/chunk";
+
 import Pages from "database/repositories/Configuration/Pages";
+import actioncards from "../components/actionCards";
 export default {
-  async beforeRouteEnter(to, from, next) {
-    let result = await Pages.getLocal();
-    next(vm => {
-      let element = vm.filterPage(result.pages, to.params.pageId);
-      vm.elements = element.innerCards;
-      console.log("vm.elements", vm.elements);
-    });
-  },
-  async beforeRouteUpdate(to, from, next) {
-    console.log("the route has changed 2");
-    next();
-  },
-  data: function() {
-    return {
-      elements: [],
-      chunkNumber: 3
-    };
-  },
   components: {
+    actioncards,
     QCard,
     QCardMedia,
     QItemSide,
@@ -101,9 +64,16 @@ export default {
     QItem,
     QItemSeparator
   },
-  computed: {
-    chunckedElements() {
-      return _chunk(this.elements, this.chunkNumber);
+  asyncData: {
+    page: {
+      async get() {
+        let result = await Pages.getLocal();
+        let element = this.filterPage(result.pages, this.$route.params.pageId);
+        return element;
+      },
+      transform(result) {
+        return result
+      }
     }
   },
   methods: {
