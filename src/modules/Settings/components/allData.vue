b89c5e39bd3037852775b23953edd4990d743cba
Ignacio Cabrera <cabrerabywaters@gmail.com>
2018-04-27 15:32:32 -0300

    adding export to all data view

--- src/modules/Settings/components/allData.vue
+++ src/modules/Settings/components/allData.vue
@@ -12,8 +12,26 @@
       <q-select filter separator autofocus-filter v-model="selectForm" :options="formList" stack-label="Select your form" filter-placeholder="Search for the form" style="border-bottom: 1px solid grey; width: 50%" clearable />
 
       <q-card-main>
+        <q-card-title>
+          <span v-if="formTitle !== ''">Form:</span> {{formTitle}}
+          <q-icon slot="right" name="more_vert" color="grey" style="cursor:pointer">
+              <q-popover ref="popover">
+                <q-list link class="no-border" dense separator no-border>
 
-        <h1 class="_control-label-title"><span v-if="formTitle !== ''">Form:</span> {{formTitle}}</h1>
+                  <q-item @click="$refs.popover.close(), createDialog()">
+                    <q-item-side icon="fa-download"  />
+                    <q-item-main :label="$t('Export')" />
+                  </q-item>
+
+                  <q-item @click="$refs.popover.close()">
+                    <q-item-side icon="fa-upload"  />
+                    <q-item-main :label="$t('Import')" />
+                  </q-item>
+
+                </q-list>
+              </q-popover>
+            </q-icon>
+        </q-card-title>
 
         <datatable
           :data="submissions"
@@ -34,18 +52,41 @@
 </template>
 
 <script>
-import { QSelect, QCardMain, QInnerLoading, QSpinnerAudio } from "quasar";
-import Form from "libraries/fastjs/database/models/Form";
-import FormioUtils from "formiojs/utils";
-import datatable from "components/dataTable/dataTable";
-import Submission from "libraries/fastjs/database/models/Submission";
-import Columns from "components/dataTable/tableFormatter/Columns";
-import Auth from "libraries/fastjs/repositories/Auth/Auth";
+import {
+  QSelect,
+  QCardMain,
+  QInnerLoading,
+  QSpinnerAudio,
+  QCardTitle,
+  QItemSide,
+  QItemMain,
+  QPopover,
+  QIcon,
+  QList,
+  QItem,
+} from 'quasar';
+import Form from 'libraries/fastjs/database/models/Form';
+import FormioUtils from 'formiojs/utils';
+import datatable from 'components/dataTable/dataTable';
+import Submission from 'libraries/fastjs/database/models/Submission';
+import Columns from 'components/dataTable/tableFormatter/Columns';
+import Auth from 'libraries/fastjs/repositories/Auth/Auth';
+import Event from "libraries/fastjs/Wrappers/Event";
+
 export default {
   async mounted() {
-    this.$eventHub.on("lenguageSelection", async data => {
+    this.$eventHub.on('lenguageSelection', async data => {
       await this.updateLocalSubmissions();
     });
+  },
+  methods: {
+      async createDialog() {
+      Event.emit({
+        name: "FAST:EXPORT:OPENMENU",
+        data: undefined,
+        text: "Triggering Open Export Menu"
+      });
+    },
   },
   asyncData: {
     formList: {
@@ -54,37 +95,37 @@ export default {
       },
       transform(result) {
         let forms = result.reduce((filtered, form) => {
-          if (form.data.tags.indexOf("visible") > -1) {
+          if (form.data.tags.indexOf('visible') > -1) {
             filtered.push({
               label: form.data.title,
-              value: form.data.path
+              value: form.data.path,
             });
           }
           return filtered;
         }, []);
         return forms;
-      }
-    }
+      },
+    },
   },
   asyncComputed: {
     currentForm: {
       get() {
-        if (this.selectForm && this.selectForm !== "") {
+        if (this.selectForm && this.selectForm !== '') {
           return Form.local().findOne({
-            "data.path": this.selectForm
+            'data.path': this.selectForm,
           });
         } else {
           return {
             data: {
-              title: ""
-            }
+              title: '',
+            },
           };
         }
-      }
+      },
     },
     submissions: {
       async get() {
-        if (this.currentForm.data.title === "") {
+        if (this.currentForm.data.title === '') {
           return [];
         }
         this.loading = true;
@@ -92,16 +133,16 @@ export default {
           form: this.currentForm.data.path,
           limit: 1000,
           select: Columns.getTableView(this.currentForm.data).map(
-            o => "data." + o.path
-          )
+            o => 'data.' + o.path,
+          ),
         };
-        if (Auth.hasRole("Reviewer")) {
+        if (Auth.hasRole('Reviewer')) {
           queryParams.filter = [
             {
-              element: "data.country",
-              query: "in",
-              value: Auth.user().data.countries
-            }
+              element: 'data.country',
+              query: 'in',
+              value: Auth.user().data.countries,
+            },
           ];
         }
 
@@ -111,45 +152,45 @@ export default {
       },
       watch() {
         this.currentForm;
-      }
-    }
+      },
+    },
   },
   computed: {
     formTitle() {
-      let title = "";
+      let title = '';
       if (this.currentForm) {
-        title = this.currentForm.data ? this.currentForm.data.title : "";
+        title = this.currentForm.data ? this.currentForm.data.title : '';
       }
       return this.$t(title);
     },
     columns() {
-      if (!this.currentForm || this.currentForm.data.title === "") {
+      if (!this.currentForm || this.currentForm.data.title === '') {
         return [
           {
-            label: "Longitude",
-            field: "longitude",
+            label: 'Longitude',
+            field: 'longitude',
             filter: true,
-            sort: true
+            sort: true,
           },
           {
-            label: "Another",
-            field: "another",
+            label: 'Another',
+            field: 'another',
             filter: true,
-            sort: true
-          }
+            sort: true,
+          },
         ];
       }
       this.visibleColumns = FormioUtils.findComponents(
         this.currentForm.data.components,
         {
           input: true,
-          tableView: true
-        }
+          tableView: true,
+        },
       );
       this.visibleColumns = this.visibleColumns.slice(0, 7);
       let columns = [];
       this.visibleColumns = this.visibleColumns.filter(c => {
-        return !!(c.label !== "");
+        return !!(c.label !== '');
       });
       this.visibleColumns.forEach((column, index) => {
         // let self = this;
@@ -157,37 +198,44 @@ export default {
           label: this.$t(column.label),
           field: column.key,
           filter: true,
-          sort: true
+          sort: true,
         };
         columns.push(visibleColum);
       });
       columns.push({
-        label: "Review",
-        field: "review",
+        label: 'Review',
+        field: 'review',
         filter: false,
-        sort: false
+        sort: false,
       });
       return columns;
-    }
+    },
   },
   components: {
     datatable,
     QSelect,
     QCardMain,
     QInnerLoading,
-    QSpinnerAudio
+    QSpinnerAudio,
+    QCardTitle,
+    QItemSide,
+    QItemMain,
+    QPopover,
+    QIcon,
+    QList,
+    QItem,
   },
   data() {
     return {
       loading: false,
       formList: [],
-      selectForm: this.$route.query.form
+      selectForm: this.$route.query.form,
     };
   },
   beforeDestroy() {
-    this.$eventHub.off("FAST-DATA_SYNCED");
-    this.$eventHub.off("FAST-DATA_IMPORTED");
-    this.$eventHub.off("lenguageSelection");
-  }
+    this.$eventHub.off('FAST-DATA_SYNCED');
+    this.$eventHub.off('FAST-DATA_IMPORTED');
+    this.$eventHub.off('lenguageSelection');
+  },
 };
 </script>
